b90c660a880e1810f5f1f9239f36edeb4945c43f
adocxwork <adipracc@gmail.com>
2026-09-24 01:44:24 +0530

    chore: Remove unused test files and Spring boilerplate

--- generate_schema.sh
+++
@@ -1,2 +0,0 @@
-mvn compile
-mvn exec:java -Dexec.mainClass="org.hibernate.tool.hbm2ddl.SchemaExport" -Dexec.classpathScope=compile
--- test-redis.java
+++
@@ -1,8 +0,0 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.UUID;
-
-public class test {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Test");
-    }
-}
